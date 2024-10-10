c60c60a1b695a0560a748007be53ea60cd380e7d
Thiago Espinhara <thiago.espinhara@greensolucoes.com.br>
2024-10-10 16:28:24 -0300

    iniciando backend
    iniciando backend

---
+++ zapdados/src/test/java/br/com/zapdados/ZapdadosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zapdados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZapdadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
