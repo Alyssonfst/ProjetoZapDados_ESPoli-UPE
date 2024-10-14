cf3cfbf5fde074056a9edb99112b3cc8d9cf8ffd
gui-carrazzoni <carrazzoni.guilherme@gmail.com>
2024-10-14 17:29:12 -0300

    Início dos graficos em relação ao tempo
    Iniciando a visualização gráfica de quantos usuários usam no dia , quantas horas cada, e os dias da semana pra cada.

--- front-zap-dados/src/app/app-routing.module.ts
+++ front-zap-dados/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RelatorioTempoUsoComponent } from './relatorio-tempo-uso/relatorio-tempo-uso.component';
+import { RelatorioExpressoesUsadasComponent } from './relatorio-expressoes-usadas/relatorio-expressoes-usadas.component';
 
 const routes: Routes = [
   { path: 'relatorio-tempo-uso', component: RelatorioTempoUsoComponent },
-
+  {path:'relatorio-expressoes-usadas', component: RelatorioExpressoesUsadasComponent}
 ];
 
 @NgModule({
--- front-zap-dados/src/app/relatorio-tempo-uso/relatorio-tempo-uso.component.ts
+++ front-zap-dados/src/app/relatorio-tempo-uso/relatorio-tempo-uso.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-relatorio-tempo-uso',
   templateUrl: './relatorio-tempo-uso.component.html',
   styleUrls: ['./relatorio-tempo-uso.component.css']
 })
-export class RelatorioTempoUsoComponent implements OnInit {
+export class RelatorioTempoUsoComponent {
+  timeOfDayChartData = [{ data: [3, 5, 7, 4], label: 'User 1', backgroundColor: '#FF6384' },
+                        { data: [1, 2, 3, 4], label: 'User 2', backgroundColor: '#36A2EB' },
+                        { data: [6, 4, 2, 1], label: 'User 3', backgroundColor: '#FFCE56' }];
+                        
+  timeOfDayChartLabels = ['0:00', '1:00', '2:00', '3:00', '4:00', '5:00', '6:00', '7:00', '8:00', '9:00', '10:00',
+                          '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', 
+                          '21:00', '22:00', '23:00'];
+  timeOfDayChartOptions = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: true, 
+      },
+    },
+    scales: {
+      x: {
+        stacked: true, 
+      },
+      y: {
+        stacked: true,
+      }
+    }
+  };
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  dayOfWeekChartData = [{ data: [3, 5, 7, 4], label: 'User 1', backgroundColor: '#FF6384' },
+                        { data: [1, 2, 3, 4], label: 'User 2', backgroundColor: '#36A2EB' },
+                        { data: [6, 4, 2, 1], label: 'User 3', backgroundColor: '#FFCE56' }];
+  dayOfWeekChartLabels = ['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado', 'Domingo'];
+  dayOfWeekChartOptions = {
+    responsive: true,
+    
+  };
 }
