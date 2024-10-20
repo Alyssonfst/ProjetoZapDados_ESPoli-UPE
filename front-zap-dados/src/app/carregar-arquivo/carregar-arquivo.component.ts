d055c1735f5131f03cf85989515a229bb43320e7
Aquelyany <mendes2075@gmail.com>
2024-10-20 08:34:40 -0300

    input para carregar arquivo movido para página home, apenas front

--- front-zap-dados/src/app/carregar-arquivo/carregar-arquivo.component.ts
+++ front-zap-dados/src/app/carregar-arquivo/carregar-arquivo.component.ts
@@ -1,15 +1,22 @@
+// carregar-arquivo.component.ts
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-carregar-arquivo',
   templateUrl: './carregar-arquivo.component.html',
-  styleUrls: ['./carregar-arquivo.component.css']
+  styleUrls: ['./carregar-arquivo.component.css'],
 })
+
 export class CarregarArquivoComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onFileSelected(event: any): void {
+    const file: File = event.target.files[0];
+    if (file) {
+      console.log('Arquivo selecionado:', file);
+      // Aqui você pode adicionar a lógica para lidar com o arquivo
+    }
   }
-
 }
