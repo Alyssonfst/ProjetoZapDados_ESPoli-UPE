c126f35cc1a855812f4a47bed2e8168124220e93
Alyssonfst <afst@poli.br>
2024-10-28 02:26:29 -0300

    Ajuste

--- zapdados/src/main/java/br/com/zapdados/service/RelatorioExpressoesService.java
+++ zapdados/src/main/java/br/com/zapdados/service/RelatorioExpressoesService.java
@@ -1,10 +1,8 @@
 package br.com.zapdados.service;
 
 import edu.stanford.nlp.pipeline.*;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
