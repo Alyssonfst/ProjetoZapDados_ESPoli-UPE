a12b8ad934e96b91e644b4b4b3dac648e6fa0c5d
gui-carrazzoni <carrazzoni.guilherme@gmail.com>
2024-11-13 23:18:31 -0300

    Relatório de mensagens funcionando
    Finalizado o primeiro a primeira versão da nossa página que demonstra as mensagens dos ultimos 30 dias por usuário

--- front-zap-dados/src/app/relatorio-ultimas-mensagens/relatorio-ultimas-mensagens.component.ts
+++ front-zap-dados/src/app/relatorio-ultimas-mensagens/relatorio-ultimas-mensagens.component.ts
@@ -13,17 +13,14 @@ export class RelatorioUltimasMensagensComponent implements OnInit {
   filteredUsers: any[] = [];
   selectedUser: string = '';
   searchTerm: string = '';
-  filteredUsersList: string[] = [];
   uniqueUsers: string[] = [];
-
+  last30DaysMessages: any[] = [];
   apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-
-    this.carregarUsuarios()
-    this.extractUniqueUsers()
+    this.carregarUsuarios();
   }
 
   carregarUsuarios(): void {
@@ -55,6 +52,7 @@ export class RelatorioUltimasMensagensComponent implements OnInit {
   handleFilterChange(username: string): void {
     this.selectedUser = username;
     this.filteredUsers = username === '' ? this.usuarios : this.usuarios.filter(usuario => usuario.username === username);
+    this.getLast30DaysMessages();
   }
 
   handleSearch(searchTerm: string): void {
@@ -63,5 +61,31 @@ export class RelatorioUltimasMensagensComponent implements OnInit {
       ? this.usuarios
       : this.usuarios.filter(usuario => usuario.username.toLowerCase().includes(searchTerm.toLowerCase()));
     this.uniqueUsers = Array.from(new Set(this.filteredUsers.map(usuario => usuario.username)));
+  }
+
+  getLast30DaysMessages(): void {
+    if (!this.selectedUser) {
+      return;
+    }
+
+    const user = this.usuarios.find(usuario => usuario.username === this.selectedUser);
+    if (!user || !user.temposUso) {
+      this.last30DaysMessages = [];
+      return;
+    }
+
+    const thirtyDaysAgo = new Date();
+    thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - 30);
+
+    this.last30DaysMessages = user.temposUso
+      .filter((tempoUso: any) => {
+        const messageDate = new Date(tempoUso.ano, tempoUso.mes - 1, tempoUso.dia);
+        return messageDate >= thirtyDaysAgo;
+      })
+      .map((tempoUso: any) => ({
+        data: new Date(tempoUso.ano, tempoUso.mes - 1, tempoUso.dia, tempoUso.horaDoDia),
+        mensagens: tempoUso.mensagens
+      }))
+      .sort((a: any, b: any) => b.data.getTime() - a.data.getTime());
   }
 }
